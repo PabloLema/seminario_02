add21c5d77e76689ddd572518119675a873ec76e
“plemasa” <pblandreslema@gmail.com>
2020-08-13 21:38:58 -0500

    final variable added

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  Preferences _preferences = Preferences();
+  final Preferences _preferences = Preferences();
   @override
   Widget build(BuildContext context) {
     print(_preferences.token);
